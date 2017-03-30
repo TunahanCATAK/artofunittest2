d2b315de26b1b9e8ffb30fcd35e2d19bac752fe7
Catak Tunahan tr1c4011 <tunahan.catak@siemens.com>
2017-03-30 12:09:04 +0200

    Chapter2-Commits

---
+++ LogAn/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        private LogAnalyzer m_analyzer = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_analyzer = new LogAnalyzer();
+        }
+
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            bool result = m_analyzer.IsValidLogFilename("filewithbadextension.foo");
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            bool result = m_analyzer.IsValidLogFilename("filewithgoodextension.slf");
+            Assert.True(result);
+        }
+    }
+}
---
+++ LogAn/LogAn/LogAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFilename(string fileName)
+        {
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+                return false;
+            else return true;
+        }
+    }
+}
